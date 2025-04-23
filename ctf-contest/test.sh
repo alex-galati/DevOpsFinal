8be23cb7084c60d6d719710803e75b24ad54cb75
alex-galati <alexanderjohn2005@gmail.com>
2025-04-22 22:36:45 -0500

    Turns out, wasn't the last. Now it is. Oops.

--- ctf-contest/test.sh
+++ ctf-contest/test.sh
@@ -3,16 +3,16 @@
 
 curlResult=$(curl -Is http://10.92.21.104:5001 | head -n 1)
 if [[ "$curlResult" == *"200 OK"* ]]; then
-    echo "red-team : service-A : passed"
+    echo "red-team     : service-A : passed"
 else
-    echo "red-team : service-A : **FAILED**"
+    echo "red-team     : service-A : **FAILED**"
 fi
 
 curlResult=$(curl -Is http://10.92.21.104:5002 | head -n 1)
 if [[ "$curlResult" == *"200 OK"* ]]; then
-    echo "red-team : service-B : passed"
+    echo "red-team     : service-B : passed"
 else
-    echo "red-team : service-B : **FAILED**"
+    echo "red-team     : service-B : **FAILED**"
 fi
 
 curlResult=$(curl -Is http://10.92.21.104:5011 | head -n 1)
